0db055e6cc16a77c98a7b080fa7feccde37bea6c
SRS <srseetharam1999@gmail.com>
2020-01-24 17:37:15 +0530

    s

--- src/config/config.dev.ts
+++ src/config/config.dev.ts
@@ -13,7 +13,7 @@ export class DevConfig extends BaseConfig {
             user: 'root',
             password: 'Karunyah',
             port: 3306,
-            database: "srs"
+            database: "we2db"
         };
     }
 
